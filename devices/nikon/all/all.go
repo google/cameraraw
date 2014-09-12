ca573e386f50afa440972aed129efd1ee8d58520
jonathanpittman <jonathanpittman@server.fake>
2014-09-12 03:23:11 -0400

    Adding nikon camera model packages to the nikon all package

--- devices/nikon/all/all.go
+++ devices/nikon/all/all.go
@@ -1,1 +1,25 @@
 package all
+
+import (
+	_ "github.com/jonathanpittman/cameraraw/devices/nikon/d1"
+	_ "github.com/jonathanpittman/cameraraw/devices/nikon/d1x"
+	_ "github.com/jonathanpittman/cameraraw/devices/nikon/d2x"
+	_ "github.com/jonathanpittman/cameraraw/devices/nikon/d3"
+	_ "github.com/jonathanpittman/cameraraw/devices/nikon/d3x"
+
+	_ "github.com/jonathanpittman/cameraraw/devices/nikon/d20"
+	_ "github.com/jonathanpittman/cameraraw/devices/nikon/d40"
+	_ "github.com/jonathanpittman/cameraraw/devices/nikon/d50"
+	_ "github.com/jonathanpittman/cameraraw/devices/nikon/d60"
+	_ "github.com/jonathanpittman/cameraraw/devices/nikon/d70"
+	_ "github.com/jonathanpittman/cameraraw/devices/nikon/d80"
+	_ "github.com/jonathanpittman/cameraraw/devices/nikon/d90"
+
+	_ "github.com/jonathanpittman/cameraraw/devices/nikon/d100"
+	_ "github.com/jonathanpittman/cameraraw/devices/nikon/d200"
+	_ "github.com/jonathanpittman/cameraraw/devices/nikon/d300"
+	_ "github.com/jonathanpittman/cameraraw/devices/nikon/d700"
+	_ "github.com/jonathanpittman/cameraraw/devices/nikon/d800"
+	_ "github.com/jonathanpittman/cameraraw/devices/nikon/d800e"
+	_ "github.com/jonathanpittman/cameraraw/devices/nikon/d810"
+)
